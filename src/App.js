6eb3bbb0a0e693cd8c8ec392b53d6238fb0ebf8b
infowaydevteam <infowaydevteam@gmail.com>
2025-02-11 04:10:58 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+import Home from '../src/Components/Home'
+
+function App() {
+  return (
+    <div className="App">
+      
+        <Home/>
+      
+    </div>
+  );
+}
+
+export default App;
